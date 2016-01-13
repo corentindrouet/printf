d2167775f80261852c2935274e4b37d7d9fb20f1
Corentin DROUET <cdrouet@e3r10p1.42.fr>
2016-01-13 15:54:14 +0100

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 11:32:20 by cdrouet           #+#    #+#             */
-/*   Updated: 2016/01/13 15:06:56 by cdrouet          ###   ########.fr       */
+/*   Updated: 2016/01/13 15:52:33 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
 
-static void		f_init(int (**f)(const char *restrict, va_list))
+static void		f_init(int (**f)(const char *reformatict, va_list))
 {
 	f[0] = &pct_s;
 	f[1] = &pct_gs;
@@ -32,33 +32,24 @@ static void		f_init(int (**f)(const char *restrict, va_list))
 	f[14] = &pct_pct;
 }
 
-static int	verif_flag(const char *restrict format, int s)
+static char	*verif_flag(const char *reformatict format, int s)
 {
-	char	*str;
-	char	*test;
 	int		i;
 
-	str = ft_strnew(ft_strlen((char*)format) + 1);
-	str = ft_strcpy(str, format);
-	test = ft_strnew(ft_strlen((char*)format) + 1);
-	test = ft_strcpy(test, "                    ");
-	test[ft_strlen((char*)format)] = '\0';
 	i = -1;
-	while (str[++i] && str[i] != s)
-		if (str[i] == '+' || str[i] == '-' || (str[i] >= '0' && str[i] <= '9') || str[i] == ' '
-			|| str[i] == 'h' || str[i] == 'l' || str[i] == 'j' || str[i] == 'z' || str[i] == '%' || str[i] == '.')
-			str[i] = ' ';
-	test[i] = str[i];
-	return (ft_strcmp(test, str));
+	while (format[++i] && format[i] != s)
+		if (format[i] != '+' || format[i] != '-' || !(format[i] >= '0' && format[i] <= '9') || format[i] != ' '
+			|| format[i] != 'h' || format[i] != 'l' || format[i] != 'j' || format[i] != 'z' || format[i] != '%' || format[i] != '.')
+			return (&format[i])
 }
 
-int			ft_printf(const char *restrict format, ...)
+int			ft_printf(const char *reformatict format, ...)
 {
 	va_list	ap;
 	int		i[3];
 	int		s;
 	char	*ptr;
-	int		(*f[15])(const char *restrict, va_list);
+	int		(*f[15])(const char *reformatict, va_list);
 
 	f_init(f);
 	ptr = "sSpdDioOuUxXcC%";
@@ -81,9 +72,9 @@ int			ft_printf(const char *restrict format, ...)
 		i[1] = 0;
 		while (ptr[i[1]] != s && ptr[i[1]])
 			i[1]++;
-		if (i[1] != 15 && !verif_flag(ft_strsub(&format[i[0]], 0, cont_carac((char*)&format[i[0]], s)), s))
+		if (i[1] != 15 && !verif_flag(ft_formatsub(&format[i[0]], 0, cont_carac((char*)&format[i[0]], s)), s))
 		{
-			i[2] += f[i[1]](ft_strsub(&format[i[0]], 0,
+			i[2] += f[i[1]](ft_formatsub(&format[i[0]], 0,
 				cont_carac((char*)&format[i[0]], s) + 1), ap);
 			i[0] += cont_carac((char*)&format[i[0]], s) + 1;
 			if (s == '%')
@@ -92,8 +83,8 @@ int			ft_printf(const char *restrict format, ...)
 		else
 			i[0]++;
 	}
-	i[2] += ft_strlen(&format[i[0]]);
-	ft_putstr(&format[i[0]]);
+	i[2] += ft_formatlen(&format[i[0]]);
+	ft_putformat(&format[i[0]]);
 	va_end(ap);
 	return (i[2]);
 }
@@ -109,7 +100,7 @@ int			cont_carac(char *s, char c)
 	return (i);
 }
 
-int			pct_pct(const char *restrict format, va_list ap)
+int			pct_pct(const char *reformatict format, va_list ap)
 {
 	(void)ap;
 	(void)format;
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 12:58:50 by cdrouet           #+#    #+#             */
-/*   Updated: 2016/01/13 15:08:00 by cdrouet          ###   ########.fr       */
+/*   Updated: 2016/01/13 15:46:23 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int		main()
 
 	i = 5;
 	//printf(" | %d\n", printf("%4.15d", -424242));
-	printf("% d\n", 9999);
+	printf("%+90.3Zoooo");
 //	ft_printf(" | %d\n", ft_printf("%4.15d", -424242));
-	ft_printf("% d\n", 9999);
+//	ft_printf("% Zoooo\n");
 	return (0);
 }
