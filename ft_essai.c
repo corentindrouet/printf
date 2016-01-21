677e833c0138f379565c13fc82be10eefeb4a480
Corentin DROUET <cdrouet@e3r10p1.42.fr>
2016-01-21 14:40:26 +0100

    printf

--- ft_essai.c
+++ ft_essai.c
@@ -6,7 +6,7 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/21 13:03:20 by cdrouet           #+#    #+#             */
-/*   Updated: 2016/01/21 13:28:58 by cdrouet          ###   ########.fr       */
+/*   Updated: 2016/01/21 14:25:09 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,39 @@ char	*precis_str(char **ptr, const char *restrict format)
 	free(*ptr);
 	if (i == 1)
 		res = ft_strjoin("-", res);
+	else if (ft_strchr(format, '+') != NULL)
+		res = ft_strjoin("+", res);
 	return (res);
 }
 
-//char	*width_str(char **ptr)
+char	*width_str(char **ptr, const char *restrict format)
+{
+	int		i;
+	int		j;
+	int		wid;
+	char	*res;
+	char	c;
+
+	i = -1;
+	while (!(format[++i] >= '0' && format[i] <= '9'))
+		if (!format[i] || format[i] == '.')
+			return (*ptr);
+	wid = ft_atoi(&format[i]);
+	c = ' ';
+	if (format[0] == '0' && ft_strchr(format, '.') == NULL)
+		c = '0';
+	if (ft_strlen(*ptr) >= wid)
+		return (*ptr);
+	res = ft_strnew(wid + 1);
+	i = 0;
+	if (((*ptr)[0] == '-' || (*ptr)[0] == '+') && ft_strchr(format, '.') == NULL
+		&& c != ' ')
+		res[i++] = (*ptr)[0];
+	j = i;
+	wid = wid - ft_strlen(&(*ptr)[i]);
+	while (i < wid)
+		res[i++] = c;
+	res = ft_strcat(res, &(*ptr)[j]);
+	free(*ptr);
+	return (res);
+}
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 12:58:50 by cdrouet           #+#    #+#             */
-/*   Updated: 2016/01/21 13:30:13 by cdrouet          ###   ########.fr       */
+/*   Updated: 2016/01/21 14:34:27 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,14 @@
 #include <locale.h>
 
 char	*precis_str(char **ptr, const char *restrict format);
+char	*width_str(char **ptr, const char *restrict format);
 
 int		main()
 {
 	char	*ptr = setlocale(LC_ALL, "");
-	char	*str;
 
-	str = ft_itoa(-1234);
-	str = precis_str(&str, "%.7d");
-	printf("|%15.7d|\n|%s|", -1234, str);
+	ft_printf(" | %d -- ft_printf\n", ft_printf("{%05p}", 0));
+	printf(" | %d -- printf\n", printf("{%05p}", 0));
 //	ft_printf(" | %d -- ft_printf\n", ft_printf("%4.1S", L"Jambon"));
 //	printf(" | %d -- printf\n", printf("%+.10d", 123456789));
 //	printf(" | %d -- printf\n", printf("%.4S", L"我是一只猫。"));
