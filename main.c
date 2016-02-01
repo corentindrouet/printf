7168f0b814a6197359308d87c0bf5552ebd55c09
Corentin DROUET <cdrouet@e3r10p1.42.fr>
2016-02-01 11:58:23 +0100

    printf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cdrouet <cdrouet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 12:58:50 by cdrouet           #+#    #+#             */
-/*   Updated: 2016/02/01 09:40:50 by cdrouet          ###   ########.fr       */
+/*   Updated: 2016/02/01 11:57:02 by cdrouet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,16 @@
 
 char	*precis_str(char **ptr, const char *restrict format);
 char	*width_str(char **ptr, const char *restrict format);
-/*
-char	*ft_dtoa(double i)
-{
 
-}
-*/
 int		main()
 {
 	char	*ptr = setlocale(LC_ALL, "");
-	ssize_t	n;
-	ssize_t	m;
+	char	str[50] = "%#.0x, %S --|%-7d, %p";
+	int		i;
 
-	ft_printf(" | %d -- ft_printf\n", ft_printf("%d-%zn-%zd", 10, &n, n));
-	printf(" | %d -- printf\n", printf("%d-%zn-%zd", 10, &m, m));
+	ft_printf("|--| retour : %d\n", ft_printf(str, 1234, L"Blabla", 1234, &i));
+	printf("|--| retour : %d\n", printf(str, 1234, L"Blabla", 1234, &i));
+//	ft_printf(" | %d -- ft_printf\n", ft_printf("%d-%zn-%zd", 10, &n, n));
+//	printf(" | %d -- printf\n", printf("%d-%zn-%zd", 10, &m, m));
 	return (0);
 }
